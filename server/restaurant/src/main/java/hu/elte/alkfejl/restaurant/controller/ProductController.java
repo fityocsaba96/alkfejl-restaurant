9086f5a2cdc1323f7b23aed285902f7534ef6cfc
marhofferbalazs <marhoffer.balazs95@gmail.com>
2017-11-01 10:33:53 +0100

    add function to create new product

--- server/restaurant/src/main/java/hu/elte/alkfejl/restaurant/controller/ProductController.java
+++ server/restaurant/src/main/java/hu/elte/alkfejl/restaurant/controller/ProductController.java
@@ -5,9 +5,7 @@ import hu.elte.alkfejl.restaurant.entity.Product;
 import hu.elte.alkfejl.restaurant.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static hu.elte.alkfejl.restaurant.entity.User.Role.ADMIN;
 import static hu.elte.alkfejl.restaurant.entity.User.Role.USER;
@@ -34,5 +32,11 @@ public class ProductController {
     private ResponseEntity<Iterable<Product>> listByCategory(@PathVariable Long id) {
         Iterable<Product> list = productService.listByCategory(id);
         return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("/products")
+    private ResponseEntity<Product> addNewProduct(@RequestBody Product product){
+        Product newProduct=productService.addNewProduct(product);
+        return ResponseEntity.ok(newProduct);
     }
 }
--- server/restaurant/src/main/java/hu/elte/alkfejl/restaurant/service/ProductService.java
+++ server/restaurant/src/main/java/hu/elte/alkfejl/restaurant/service/ProductService.java
@@ -1,6 +1,7 @@
 package hu.elte.alkfejl.restaurant.service;
 
 import hu.elte.alkfejl.restaurant.entity.Product;
+import hu.elte.alkfejl.restaurant.repository.CategoryRepository;
 import hu.elte.alkfejl.restaurant.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +16,21 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     public Iterable<Product> list() {
         return productRepository.findAll();
     }
 
     public Iterable<Product> listByCategory(Long id) {
         return productRepository.findAllByCategory_Id(id);
+    }
+
+    public Product addNewProduct(Product product){
+        if(product.getCategory().getId()!=null){
+            product.setCategory(categoryRepository.findOne(product.getCategory().getId()));
+        }
+        return productRepository.save(product);
     }
 }
